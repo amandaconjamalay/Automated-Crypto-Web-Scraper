951ece7203084396a91a4a7bbdda2b516ab52f21
Amanda <59309486+amandaconjamalay@users.noreply.github.com>
2025-10-18 15:13:20 +0400

    Add files via upload

---
+++ Automated Web Scraper.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+import os
+from datetime import datetime
+import time 
+
+
+# In[ ]:
+
+
+def automated_crypto_pull():
+    # Getting the url of the website we want to scrape.
+    url = 'https://coinmarketcap.com/currencies/bitcoin/'
+    # Making sure the respinse is good.
+    page = requests.get(url)
+    # getting text from the website.
+    soup = BeautifulSoup(page.text, 'html')
+
+    # Retrieving the title 'Bitcoin' from html script.
+    crypto_name = soup.find('span', class_ = 'sc-65e7f566-0 lsTl').contents[0]
+    # Retrieving the crypto price from html script.
+    crypto_price = soup.find('span', attrs={"data-test": "text-cdp-price-display"}).text
+    # Removing '$' sign from the crypto price.
+    final_crypto_price = crypto_price.replace('$', '')
+
+    # Current date and time.
+    date_time = datetime.now()
+
+    # Creating a dictionary to structure information.
+    dict = {'Crypto Name': crypto_name, 
+            'Price' : final_crypto_price,
+            'TimeStamp': date_time}
+
+    # Converting dictonary to dataframe
+    df = pd.DataFrame([dict])
+
+    # If file exists in file path add info to existing file, else create csv. 
+    if os.path.exists(r'</FilePath/'):
+        df.to_csv(r'</FilePath/', mode='a', header= False, index = False)
+    else:
+        df.to_csv(r'</FilePath/', index = False)
+    # So we can have tab open without it erroring out
+    print(df)
+
+
+# In[ ]:
+
+
+# While function is true keeping looping every hour.
+while True: 
+    automated_crypto_pull()
+    # Run cell after a certain amount of seconds
+    time.sleep(3600)
+
+
+# In[ ]:
+
+
+
+
